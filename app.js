099d6c68564adfbd2b747ef9fb392dbc8e9ee7a6
Karen Pinto <konpinto@gmail.com>
2015-02-12 01:35:05 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express=require("express");
+var app = express();
+var fs=require('fs');
+
+var port = process.env.PORT || 5000
+var express = require('express')
+var app = express()
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', function(req, res){
+    fs.readFile(__dirname + '/index.html',function (err,data){
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+app.listen(port, function () {
+    console.log('listening on *',port);
+})
